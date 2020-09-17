7aa04bf7e9bdd5090b738086d44c346b43a98941
Erika Zibelnik <e.m.zibelnik@gmail.com>
2020-09-17 17:35:12 -0400

    updating

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 $("#currentDay").append(moment().format("dddd, MMMM Do h:mm:ss a"));
+console.log(moment().format("dddd, MMMM Do h:mm:ss a"));
+$("#one").append(moment().hour());
+console.log(moment().hour());
+
+// var hour = $(".hour").text()
+
+// var hourArray = [];
+
+// function time() {
+//     for (let i = 0; i < array.length; i++) {
+//         const element = array[index];
+        
+//     }
+// }
