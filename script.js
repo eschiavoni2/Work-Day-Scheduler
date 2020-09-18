41b22514c80db7cfdc1bb490ee22d1e4cff1eb5c
Erika Zibelnik <e.m.zibelnik@gmail.com>
2020-09-18 10:36:32 -0400

    Got help from John with my html and js, added text area in html

--- script.js
+++ script.js
@@ -1,8 +1,30 @@
+// load document first before jquery
+$( document ).ready(function() {
+// getting current time/day appended to page
 $("#currentDay").append(moment().format("dddd, MMMM Do h:mm:ss a"));
-console.log(moment().format("dddd, MMMM Do h:mm:ss a"));
 
-$("val").append(moment().hour());
-console.log(moment().hour());
+// variable for current time
+var currentTime = moment().hour();
 
 
-var currentTime = moment().hour()
+    function timeCheck() {
+        $(".row").each(function () {
+            var hour = parseInt($(this).attr("id"))
+            console.log(hour)
+            // check if we’ve moved past this time
+            if (hour < currentTime) {
+                $(this).addClass("past");
+            }
+            else if (hour === currentTime) {
+                $(this).removeClass("past");
+                $(this).addClass("present");
+            }
+            else {
+                $(this).removeClass("past");
+                $(this).removeClass("present");
+                $(this).addClass("future");
+            }
+        });
+    }
+    timeCheck();
+});
