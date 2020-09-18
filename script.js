d17ac1edd2db1e39b5cfe139f395810521ed41ce
Erika Zibelnik <e.m.zibelnik@gmail.com>
2020-09-17 20:40:05 -0400

    changed value to be military value

--- script.js
+++ script.js
@@ -1,15 +1,8 @@
 $("#currentDay").append(moment().format("dddd, MMMM Do h:mm:ss a"));
 console.log(moment().format("dddd, MMMM Do h:mm:ss a"));
-$("#one").append(moment().hour());
+
+$("val").append(moment().hour());
 console.log(moment().hour());
 
-var hour = $(".hour").text();
-var hourArray = [];
 
-i = 0;
-
-function hourV() {
-    console.log(hourArray);
-    hourArray.push(hour);
-}
-hourV();
+var currentTime = moment().hour()
